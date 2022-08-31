bac9ef87baccb9d433ccef958b6fd52e7f09758b
DanielHorbunov <111472364+DanielHorbunov@users.noreply.github.com>
2022-08-31 11:26:43 +0300

    Attempt to overcome date format issue. See iso_date_parser function for details

--- thebeast/contrib/transformers/__init__.py
+++ thebeast/contrib/transformers/__init__.py
@@ -6,6 +6,8 @@ from names_translator.name_utils import try_to_fix_mixed_charset, parse_and_gene
 # TODO: split into dates/names/others files
 
 
+
+
 def mixed_charset_fixer(values: List[str]) -> List[str]:
     return [try_to_fix_mixed_charset(value) for value in values]
 
@@ -16,6 +18,24 @@ def anydate_parser(values: List[str]) -> List[str]:
 
 def anydatetime_parser(values: List[str]) -> List[str]:
     return [str(dt_parse(value)) for value in values]
+
+
+# Transforms strings of the form \w{2}.\w{2}.\w{4} to dates as strings, expressed in ISO-8601 format:
+# xx.tt.ssss -> yyyy-mm-dd
+# There is no additional verification on digits.
+# ftm comprehends date in mentioned format. If there are any inconsistencies, it will warn us.
+def date_parser_helper(value: str) -> str:
+    to_return = []
+    # iterate through components of string starting from the end
+    for elem in value.split(".")[::-1]:
+        # if elem contains digits, append to other components, else transform to "-"
+        to_return.append(elem if str.isdigit(elem) else "-")
+    return "-".join(to_return)
+
+
+def iso_date_parser(values: List[str]) -> List[str]:
+    if(len(values) > 1): print(values)
+    return [date_parser_helper(value) for value in values]
 
 
 def names_transliteration(values: List[str]) -> List[str]:
